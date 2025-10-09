81be83c2be58fab14ec15ce0970d3fb364dbf9cd
Bongo117 <luchosoto117@gmail.com>
2025-10-09 03:27:40 -0300

    Implementacion de un dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,42 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Inmobiliaria_Grippo_Soto.Models;
+using Inmobiliaria_.Net_Core.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Inmobiliaria_Grippo_Soto.Controllers;
 
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly RepositorioInmueble _repoInmueble;
+    private readonly RepositorioContrato _repoContrato;
+    private readonly RepositorioInquilino _repoInquilino;
+    private readonly RepositorioPropietario _repoPropietario;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, RepositorioInmueble repoInmueble, RepositorioContrato repoContrato, RepositorioInquilino repoInquilino, RepositorioPropietario repoPropietario)
     {
         _logger = logger;
+        _repoInmueble = repoInmueble;
+        _repoContrato = repoContrato;
+        _repoInquilino = repoInquilino;
+        _repoPropietario = repoPropietario;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var todosLosInmuebles = _repoInmueble.ObtenerTodos();
+        var viewModel = new DashboardViewModel
+        {
+            InmueblesDisponibles = todosLosInmuebles.Count(i => i.Estado),
+            TotalInmuebles = todosLosInmuebles.Count,
+            ContratosVigentes = _repoContrato.ObtenerVigentes().Count,
+            InquilinosActivos = _repoInquilino.ObtenerTodos().Count,
+            PropietariosActivos = _repoPropietario.ObtenerTodos().Count,
+            ContratosPorVencer = _repoContrato.ObtenerTodos().Where(c => c.Estado && c.FechaFin >= DateTime.Today && c.FechaFin <= DateTime.Today.AddDays(30)).ToList()
+        };
+        return View(viewModel);
     }
 
     public IActionResult Privacy()
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+namespace Inmobiliaria_.Net_Core.Models
+{
+    public class DashboardViewModel
+    {
+        public int InmueblesDisponibles { get; set; }
+        public int TotalInmuebles { get; set; }
+        public int ContratosVigentes { get; set; }
+        public int InquilinosActivos { get; set; }
+        public int PropietariosActivos { get; set; }
+        public List<Contrato> ContratosPorVencer { get; set; } = new List<Contrato>();
+    }
+}
